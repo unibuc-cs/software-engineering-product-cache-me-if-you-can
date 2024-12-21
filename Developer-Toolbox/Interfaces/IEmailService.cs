24b82876b7e91489756928cde88bfacb9fdddcb6
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2024-12-21 19:57:42 +0200

    added email notifications without question/answer deleted

--- Developer-Toolbox/Interfaces/IEmailService.cs
+++ Developer-Toolbox/Interfaces/IEmailService.cs
@@ -1,9 +1,12 @@
-﻿namespace Developer_Toolbox.Interfaces
+﻿using Developer_Toolbox.Models;
+
+namespace Developer_Toolbox.Interfaces
 {
     public interface IEmailService
     {
-        Task SendBadgeAwardedEmailAsync(string userEmail, string userName, string badgeName);
-        Task SendChallengeStartedEmailAsync(string userEmail, string userName, string challengeName, DateTime endDate);
-        Task SendChallengeEndedEmailAsync(string userEmail, string userName, string challengeName);
+        Task SendBadgeAwardedEmailAsync(string userEmail, string userName, Badge badge);
+/*        Task SendNewChallengeEmailAsync(string userEmail, string userName, WeeklyChallenge challenge);*/
+        Task SendAnsweredReceivedEmailAsync(string userEmail, string userName, Question question);
+        Task SendContentDeletedByAdminEmailAsync(string userEmail, string userName, string deletedContent);
     }
 }
