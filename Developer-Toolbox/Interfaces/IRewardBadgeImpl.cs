f03a977b34b1fbee446ad1a3466ce871cb5d6873
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2024-12-28 20:01:30 +0200

    fixed reward complete challenge

--- Developer-Toolbox/Interfaces/IRewardBadgeImpl.cs
+++ Developer-Toolbox/Interfaces/IRewardBadgeImpl.cs
@@ -237,30 +237,48 @@ namespace Developer_Toolbox.Interfaces
 
         public void RewardCompleteChallengeBadge(Badge badge, string userId)
         {
-            // take all exercises ids of the challenges targetted in the badge
-            var exercisesToBeSolved = from bc in db.BadgeChallenges
-                                      join ce in db.WeeklyChallengeExercises on bc.WeeklyChallengeId equals ce.WeeklyChallengeId
-                                      where bc.BadgeId == badge.Id
-                                      select ce.ExerciseId;
-
-            // take all 100 scored usere's submissions of the exercises
-            var submissions = from s in db.Solutions
-                              join e in exercisesToBeSolved on s.ExerciseId equals e
-                              where s.UserId == userId && s.Score == 100
-                              group s by e into g
-                              select g.Count();
-
-            // if there is at least one 100 scored submission of every exercise, than the user has completed the challenge
-            if (submissions.Count(g => g >= 1) == exercisesToBeSolved.Count()) {
-
-                // assign badge
-                db.UserBadges.Add(new UserBadge
-                {
-                    UserId = userId,
-                    BadgeId = badge.Id,
-                    ReceivedAt = DateTime.Now
-                });
+            if (!(badge.SelectedChallengesIds != null && badge.SelectedChallengesIds.Any()))
+            {
+                return;
             }
+
+            foreach (var challengeId in badge.SelectedChallengesIds)
+            {
+                var challenge = db.WeeklyChallenges.Find(challengeId);
+
+                // Lista de ExerciseIds asociate acestui WeeklyChallenge
+                var exerciseIds = from ce in db.WeeklyChallengeExercises
+                                  where ce.WeeklyChallengeId == challengeId
+                                  select ce.ExerciseId;
+
+                // Numărul total de exerciții asociate provocării
+                var nrTotal = exerciseIds.Count();
+
+                // Filtrăm soluțiile pentru a le lua doar pe cele care au data CreatedAt între StartDate și EndDate
+                var nrSolutii = db.Solutions
+                    .Where(s => s.UserId == userId
+                        && s.Score == 100
+                        && s.ExerciseId.HasValue
+                        && exerciseIds.Contains(s.ExerciseId.Value)
+                        && s.CreatedAt >= challenge.StartDate.Date
+                        && s.CreatedAt <= challenge.EndDate.AddDays(1).Date)
+                    .Select(s => s.ExerciseId)
+                    .Distinct() 
+                    .Count();
+
+                if (nrSolutii < 1)
+                {
+                    return;
+                } 
+            }
+
+            // assign badge
+            db.UserBadges.Add(new UserBadge
+            {
+                UserId = userId,
+                BadgeId = badge.Id,
+                ReceivedAt = DateTime.Now
+            });
 
             db.SaveChanges();
         }
