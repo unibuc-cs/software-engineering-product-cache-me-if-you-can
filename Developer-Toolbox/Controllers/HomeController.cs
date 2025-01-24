30ff8391c2e764085e07f058d40ea0d305ba14b8
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2025-01-24 19:32:54 +0200

    added community menu

--- Developer-Toolbox/Controllers/HomeController.cs
+++ Developer-Toolbox/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace Developer_Toolbox.Controllers
             return View();
         }
 
+        public IActionResult Community()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
