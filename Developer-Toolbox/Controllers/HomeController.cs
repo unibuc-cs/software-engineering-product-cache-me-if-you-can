604646eefa4f3d7c084d3a02cfea10e51c6ed042
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2025-01-25 12:17:58 +0200

    added a toolbox features menu and started changes to navbar

--- Developer-Toolbox/Controllers/HomeController.cs
+++ Developer-Toolbox/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Developer_Toolbox.Controllers
             return View();
         }
 
+        public IActionResult Toolbox()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
